4adf9dd196b1a03784722b7d9d4a3a2815ce56e5
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2023-05-15 18:16:49 +0200

    tweaks

--- main.js
+++ main.js
@@ -4,36 +4,39 @@ const animation = document.getElementById("startAnimation");
 const phoneblock = document.getElementById("phoneblock");
 const animationContext = animation.getContext("2d");
 
-window.addEventListener('resize', resizeCanvas, false);
-window.addEventListener('orientationchange', resizeCanvas, false);
+window.addEventListener("resize", resizeCanvas, false);
+window.addEventListener("orientationchange", resizeCanvas, false);
 resizeCanvas();
 
 function resizeCanvas() {
   animation.width = screen.width;
   animation.height = screen.height;
   phoneblock.style.display = "none";
-  if (navigator.userAgent.match(/Android/i)
-  || navigator.userAgent.match(/webOS/i)
-  || navigator.userAgent.match(/iPhone/i)
-  || navigator.userAgent.match(/iPad/i)
-  || navigator.userAgent.match(/iPod/i)
-  || navigator.userAgent.match(/BlackBerry/i)
-  || navigator.userAgent.match(/Windows Phone/i)) {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
     animation.width = screen.width;
-    animation.height = screen.height/2;
+    animation.height = screen.height / 2;
     phoneblock.style.display = "block";
   }
 }
 
 const animationInfo = {
-  totalFrames:99,
-  totalTime:50,
+  totalFrames: 99,
+  totalTime: 50,
   images: [],
-  currentFrame:0,
-  currentImage: (index) => `./assets/animation/hexagon${index.toString().padStart(4,"0")}.png`,
+  currentFrame: 0,
+  currentImage: (index) =>
+    `./assets/animation/hexagon${index.toString().padStart(4, "0")}.png`,
 };
 const img = new Image();
-for(let i = 1; i <= animationInfo.totalFrames; i++) {
+for (let i = 1; i <= animationInfo.totalFrames; i++) {
   const img = new Image();
   img.src = animationInfo.currentImage(i);
   animationInfo.images.push(img);
@@ -51,21 +54,22 @@ gsap.to(animationInfo, {
   },
   onUpdate: render,
 });
-animationInfo.images[0].onload = ()=>{
-  animationContext.drawImage(animationInfo.images[0],   
+animationInfo.images[0].onload = () => {
+  animationContext.drawImage(
+    animationInfo.images[0],
     0,
-    0,    
+    0,
     animation.width,
     animation.height
-    );
-}
+  );
+};
 
-function render(){
+function render() {
   animationContext.drawImage(
     animationInfo.images[animationInfo.currentFrame],
     0,
     0,
     animation.width,
     animation.height
-    );
+  );
 }

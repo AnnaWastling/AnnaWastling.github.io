a37d0058df5b8c6a31aefe64aff84b34b1838f3a
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2023-05-19 13:26:30 +0200

    fixed phone version

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const animation = document.getElementById("startAnimation");
-const phoneblock = document.getElementById("phoneblock");
+const phone = document.getElementById("phone");
+const desktop = document.getElementById("desktop");
 const animationContext = animation.getContext("2d");
+
+const animationInfo = {
+  totalFrames: 99,
+  totalTime: 50,
+  images: [],
+  currentFrame: 0,
+  currentImage: (index) =>
+    `./assets/animation/hexagon${index.toString().padStart(4, "0")}.png`,
+};
 
 window.addEventListener("resize", resizeCanvas, false);
 window.addEventListener("orientationchange", resizeCanvas, false);
@@ -11,7 +21,9 @@ resizeCanvas();
 function resizeCanvas() {
   animation.width = screen.width;
   animation.height = screen.height;
-  phoneblock.style.display = "none";
+  desktop.style.display = "block";
+  phone.style.display = "none";
+  renderAnimation();
   if (
     navigator.userAgent.match(/Android/i) ||
     navigator.userAgent.match(/webOS/i) ||
@@ -23,24 +35,19 @@ function resizeCanvas() {
   ) {
     animation.width = screen.width;
     animation.height = screen.height / 2;
-    phoneblock.style.display = "block";
+    phone.style.display = "block";
+    desktop.style.display = "none";
   }
 }
 
-const animationInfo = {
-  totalFrames: 99,
-  totalTime: 50,
-  images: [],
-  currentFrame: 0,
-  currentImage: (index) =>
-    `./assets/animation/hexagon${index.toString().padStart(4, "0")}.png`,
-};
-const img = new Image();
+function renderAnimation(){
+  const img = new Image();
 for (let i = 1; i <= animationInfo.totalFrames; i++) {
   const img = new Image();
   img.src = animationInfo.currentImage(i);
   animationInfo.images.push(img);
 }
+
 gsap.to(animationInfo, {
   currentFrame: animationInfo.totalFrames,
   snap: "currentFrame",
@@ -54,6 +61,8 @@ gsap.to(animationInfo, {
   },
   onUpdate: render,
 });
+}
+
 animationInfo.images[0].onload = () => {
   animationContext.drawImage(
     animationInfo.images[0],
